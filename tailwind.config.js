28d9c5506c2d2f5ea0bbdebb47f88ddd016eb0b5
hong0121 <94957076+hong0121@users.noreply.github.com>
2024-07-10 11:31:19 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  
+  screen: {
+    'desktop' : '1080px'
+  },
+
+  color: {
+    
+  },
+
+  theme: {
+    fontFamily: {
+      sans: ['"Noto Sans KR"', 'Roboto', 'sans-serif']
+    },
+    extend: {
+      
+    },
+  },
+
+  plugins: [],
+  
+}
